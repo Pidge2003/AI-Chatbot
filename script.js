5a0346ada9437755919a05f2a32a18967bdf8378
Luke Partridge <lukej.partridge@icloud.com>
2025-03-04 20:24:02 +0000

    Added JS that creates the users message within the chat

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 const messageInput = document.querySelector(".message-input");
+const chatBody = document.querySelector(".chat-body");
+
+// Create message element with dynamic classes
+const createMessageElement = (content, classes) => {
+    const div = document.createElement("div");
+    div.classList.add("message", classes);
+    div.innerHTML = content;
+    return div;
+}
+
+// Handles Outoging User messages
+const handleOutgoingMessage = (userMessage) => {
+    const messageContent = `<div class="message-text">${userMessage}</div>`;
+    createMessageElement(messageContent, "user-message");
+    const outgoingMessageDiv = createMessageElement(messageContent, "user-message");
+    chatBody.appendChild(outgoingMessageDiv);
+}
 
 // Handles message sending when pressing enter key
 messageInput.addEventListener("keydown", (e) => {
     const userMessage = e.target.value.trim();
     if(e.key === "Enter" && userMessage) {
-        console.log(userMessage);
+        handleOutgoingMessage(userMessage);
     }
 });
