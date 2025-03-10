fd8f475b4a2f3327f9033913d7bfb2cdb584e50d
Luke Partridge <lukej.partridge@icloud.com>
2025-03-10 23:42:12 +0000

    Following API docs, you can now select and input emojis

--- script.js
+++ script.js
@@ -143,6 +143,11 @@ const picker = new EmojiMart.Picker({
     theme: "light",
     skinTonePosition: "none",
     previewPosition: "none",
+    onEmojiSelect: (emoji) => {
+        const {selectionStart: start, selectionEnd: end } = messageInput;
+        messageInput.setRangeText(emoji.native, start, end, "end");
+        messageInput.focus();
+    },
     onClickOutside: (e) => {
         if(e.target.id === "emoji-picker") {
             document.body.classList.toggle("show-emoji-picker");
